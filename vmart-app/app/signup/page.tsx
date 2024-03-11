71c2d27062e441bbd52a35877f16f8bf494fc4d5
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-03-11 18:47:29 +0530

    :white_check_mark: created sign up page.

--- vmart-app/app/signup/page.tsx
+++ vmart-app/app/signup/page.tsx
@@ -5,14 +5,25 @@ import { useState } from 'react';
 import { RiEyeCloseFill } from 'react-icons/ri';
 import { HiEye } from 'react-icons/hi2';
 
-export default function Login() {
+export default function Signup() {
   const [toggleEye, setToggleEye] = useState(false);
 
   return (
     <section className="min-h-screen bg-primary flex items-center textAqua justify-center">
       <div className="max-w-md w-full p-6 border-2 border-[--bbg] rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold  mb-6 text-center">Login</h1>
+        <h1 className="text-2xl font-bold  mb-6 text-center">SignUp</h1>
         <form>
+          <div className="mb-4">
+            <label className="block  font-bold mb-2" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="appearance-none input border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+              id="name"
+              type="text"
+              placeholder="Enter your name"
+            />
+          </div>
           <div className="mb-4">
             <label className="block  font-bold mb-2" htmlFor="email">
               Email
@@ -53,15 +64,15 @@ export default function Login() {
               className="hover:bg-[--bbg] hover:text-black border-2 border-[--bbg] font-semibold py-2 px-4 rounded"
               type="submit"
             >
-              Login
+              Sign Up
             </button>
             <p className="">
-              Not a User?{' '}
+              Already a User?{' '}
               <Link
-                href="/signup"
+                href="/login"
                 className="hover:underline underline-offset-4"
               >
-                Sign Up
+                Login
               </Link>
             </p>
           </div>
